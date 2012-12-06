e6e16f3a6111d93a200729c32b1063f29bc339d3
Shiti <ssaxena.ece@gmail.com>
2012-12-06 12:32:08 +0530

    Reverting files to take pull from origin

--- app/js/app.js
+++ app/js/app.js
@@ -1,18 +1,3 @@
 'use strict';
 
-var quizApp = angular.module('quizApp', ['ngResource']);
-
-quizApp.filter('startFrom', function () {
-    return function (input, start) {
-        var result = [];
-        start = +start;
-        if (input) {
-            result = input.slice(start);
-        }
-        return result;
-    };
-});
-
-quizApp.config(function ($routeProvider) {
-    $routeProvider.when('/', { template: 'js/views/templates/quizView.html'});
-});
+var quizApp = angular.module('quizApp', ['ngResource', 'components']);

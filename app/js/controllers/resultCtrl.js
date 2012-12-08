b31eadf606edbe5a45315e036834865c54e37756
Shiti Saxena <ssaxena.ece@gmail.com>
2012-12-08 09:44:43 +0530

    removed IDE details

--- app/js/controllers/resultCtrl.js
+++ app/js/controllers/resultCtrl.js
@@ -1,10 +1,3 @@
-/**
- * Created with IntelliJ IDEA.
- * User: shiti
- * Date: 12/8/12
- * Time: 7:55 AM
- * To change this template use File | Settings | File Templates.
- */
 "use strict";
 
 quizApp.controller('ResultCtrl', function HomeCtrl($rootScope, $scope) {

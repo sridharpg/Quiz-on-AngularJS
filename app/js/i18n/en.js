3083729a9ad9b02cced87dd940eef902a9d4d7d0
kiranp <kiranraop1989@gmail.com>
2012-12-11 14:00:07 +0530

    changes done to implement i18n

---
+++ app/js/i18n/en.js
@@ -0,0 +1,8 @@
+﻿{
+    "locale-id": "en",
+    "appTitle": "Quiz Application",
+    "home-title": "Quiz",
+    "home-text": "Enter your name",
+    "start-quiz": "Start Quiz",
+    "help": "Help"
+}

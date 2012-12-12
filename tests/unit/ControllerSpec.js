9375369c413a37dfd41e274a0005e29a8feb5426
Shiti Saxena <ssaxena.ece@gmail.com>
2012-12-12 11:16:17 +0530

    updated test cases for home controller

--- tests/unit/ControllerSpec.js
+++ tests/unit/ControllerSpec.js
@@ -3,17 +3,25 @@ describe('App controllers', function () {
     beforeEach(module('quizApp'));
 
     describe('HomeCtrl', function () {
-        var scope, ctrl, $httpBackend;
+        var scope, ctrl;
 
         beforeEach(inject(function ($rootScope, $controller, $location) {
             scope = $rootScope.$new();
-            scope.userName = "testUser";
             ctrl = $controller('HomeCtrl', {$scope: scope, $rootScope: $rootScope, $location: $location});
         }));
 
         it("should be valid name", function () {
+            scope.userName = "testUser";
             expect(scope.isValidUserName).toBeTruthy();
         });
+
+        it("should ignore all spaces as name", function () {
+            scope.userName = "      ";
+            expect(scope.isValidUserName).toBeFalsy();
+        });
+
     });
+
+
 });
 

c1ac72198383302b399b3f9e1134545890641e30
Shiti Saxena <ssaxena.ece@gmail.com>
2012-12-12 20:03:05 +0530

    updated Result view

--- app/js/controllers/resultCtrl.js
+++ app/js/controllers/resultCtrl.js
@@ -1,6 +1,19 @@
 "use strict";
 
-quizApp.controller('ResultCtrl', function HomeCtrl($rootScope, $scope) {
+quizApp.controller('ResultCtrl', function HomeCtrl($rootScope, $scope, $location) {
     $scope.user = $rootScope.user;
     $scope.quizSize = $rootScope.quizSize;
+
+    $scope.playAgain = function () {
+        $rootScope.user = {};
+        $rootScope.quizSize = 0;
+        $location.path('/quiz');
+    };
+
+    $scope.newGame = function () {
+        $rootScope.user = {};
+        $rootScope.userName = "";
+        $rootScope.quizSize = 0;
+        $location.path('/');
+    };
 });

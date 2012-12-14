8dedaa988db358834b0dbd3be5200462fa5aff39
kiranp <kiranraop1989@gmail.com>
2012-12-14 15:57:57 +0530

    i18n implemented for quizView and helpView changed

--- app/js/i18n/de.js
+++ app/js/i18n/de.js
@@ -1,13 +1,19 @@
 ﻿{
     "locale-id": "de",
     "appTitle": "Quiz Anwendung",
-    "home-title": "Quiz",
-    "home-text": "geben Sie Ihren Namen",
+    "Quiz": "Quiz",
+    "enter-your-name": "Geben Sie Ihren Namen",
     "start-quiz": "Quiz starten",
     "help": "helfen",
     "results":"Ergebnisse",
     "congrats":"Herzlichen Glückwunsch!",
     "score-msg":"Ihr Ergebnis ist ",
     "play-again":"spiele wieder",
-    "new-game":"neues Spiel"
+    "new-game":"neues Spiel",
+    "next":"neben",
+    "pass":"gehen",
+    "question":"Frage",
+    "weight":"Gewicht",
+    "quit":"verlassen",
+    "back":"zurück"
 }

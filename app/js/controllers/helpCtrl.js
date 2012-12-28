aa5264a60d6b9cd1c76764e43739054700067c6b
Shiti Saxena <ssaxena.ece@gmail.com>
2012-12-28 12:07:16 +0530

    updated help view

--- app/js/controllers/helpCtrl.js
+++ app/js/controllers/helpCtrl.js
@@ -1,7 +1,12 @@
 "use strict";
 
-quizApp.controller('HelpCtrl', function HelpCtrl($scope, $location) {
-    $scope.back = function(){
+quizApp.controller('HelpCtrl', function HelpCtrl($scope, $resource, quizModel, $location) {
+    $resource('fixtures/questions.json').get(function (data) {
+        var quiz = quizModel.initialize(data);
+        $scope.quizSize = quiz.questionnaire.length;
+        $scope.duration = quiz.duration;
+    });
+    $scope.back = function () {
         $location.path('/');
-    }
+    };
 });
